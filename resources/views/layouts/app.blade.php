ce98172f90668353c31dcc0e4daf6bee7bd2ed4f
bobo@qq.com <bobo_pws@qq.com>
2017-11-28 13:53:33 +0800

    created topics by user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,27 +8,27 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'dream')</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
-<div id="app" class="{{ route_class() }}-page">
-    @include('layouts._header')
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
 
-    <div class="container">
-        @include('layouts._message')
+        <div class="container">
+            @include('layouts._message')
 
-        @yield('content')
+            @yield('content')
 
+        </div>
+
+        @include('layouts._footer')
     </div>
 
-    @include('layouts._footer')
-</div>
-
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
